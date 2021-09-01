343a4b31ffc7d4e00057e3cb58c335c3c40e175a
Mogo <cy1mxc@outlook.com>
2021-09-01 17:42:28 +0800

    跨域解决：代理

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/driverexamQuery': {
+        target:
+          'https://way.jd.com//jisuapi/driverexamQuery?appkey=e6dab253d0240567bd548823256a7f20&', // 真实请求的地址
+        changeOrigin: true, // 是否跨域
+        pathRewrite: {
+          '^/driverexamQuery': ''
+        }
+      }
+    }
+  }
+}
